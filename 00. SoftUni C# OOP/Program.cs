34b738372cc51539affca4570a6d67862f816919
Valentin Karakashev <116033694+ValchoPalcho@users.noreply.github.com>
2024-03-12 13:40:42 +0200

    Add project files.

---
+++ 00. SoftUni C# OOP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00._SoftUni_C__OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
